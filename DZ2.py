456c8e69f95f33d37bb8f7d32c904869b3acb256
Choppa <denis.afinogenow@yandex.ru>
2022-09-15 20:00:10 +0300

    исправил задачу

--- DZ2.py
+++ DZ2.py
@@ -1,10 +1,14 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
-N = int(input())
-
-list = []
-
-for i in range(1,N+1):
-    if N % i == 0:
-        list.append(i)
-print(list)
+num = int(input("Введите число: "))
+i = 2 # первое простое число
+lst = []
+old = num
+while i <= num:
+    if num % i == 0:
+        lst.append(i)
+        num //= i
+        i = 2
+    else:
+        i += 1
+print(f"Простые множители числа {old} приведены в списке: {lst}")
